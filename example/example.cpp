a284b284800add49873d7f17fc093741d3ff99d1
patryk411 <117459014+patryk411@users.noreply.github.com>
2022-11-04 15:18:34 +0100

    Adds an empty C++
    Empty Dev-C++ project

---
+++ example/example.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+string imie, nazwisko;
+
+int nr_tel;
+
+int main(int argc, char** argv)
+{
+	fstream plik;
+	plik.open("wizytowka.txt",ios::in);
+	
+	if(plik.good()==false)
+	{
+		cout << "Plik nie istnieje";
+		exit(0);
+	}
+	
+	string linia;
+	int nr_linii = 1;
+	
+	while(getline(plik, linia))
+	{
+		switch(nr_linii)
+		{
+			case 1: 
+			{
+				imie;
+			}
+			break;
+			
+			case 2:
+			{
+				nazwisko;
+			}
+			break;
+			
+			case 3:
+			{
+				nr_tel=atoi(linia.c_str());
+			}
+			break;
+		}
+		
+		nr_linii++;
+	}
+
+    plik.close();
+    
+    	
+    cout<<imie<<endl;
+	cout<<nazwisko<<endl;
+	cout<<nr_tel<<endl;
+	
+	return 0;
+}
